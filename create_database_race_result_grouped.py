e095b2fd4f2f19ae30b08ba7a170b89a7bfa1a3b
sheepover96 <sheep.over@gmail.com>
2019-10-20 15:57:19 +0900

    grouped result creation bug corrected

--- create_database_race_result_grouped.py
+++ create_database_race_result_grouped.py
@@ -27,35 +27,35 @@ def main():
         lane5_racer_id = int(data_row['lane5_ID'])
         lane6_racer_id = int(data_row['lane6_ID'])
 
-        if np.isnan(data_row['result_rank1_lane']):
+        if np.isnan(data_row['lane1_result_rank']):
             lane1_result_rank = None
         else:
-            lane1_result_rank = int(data_row['result_rank1_lane'])
+            lane1_result_rank = int(data_row['lane1_result_rank'])
 
-        if np.isnan(data_row['result_rank2_lane']):
+        if np.isnan(data_row['lane2_result_rank']):
             lane2_result_rank = None
         else:
-            lane2_result_rank = int(data_row['result_rank2_lane'])
+            lane2_result_rank = int(data_row['lane2_result_rank'])
 
-        if np.isnan(data_row['result_rank3_lane']):
+        if np.isnan(data_row['lane3_result_rank']):
             lane3_result_rank = None
         else:
-            lane3_result_rank = int(data_row['result_rank3_lane'])
+            lane3_result_rank = int(data_row['lane3_result_rank'])
 
-        if np.isnan(data_row['result_rank4_lane']):
+        if np.isnan(data_row['lane4_result_rank']):
             lane4_result_rank = None
         else:
-            lane4_result_rank = int(data_row['result_rank4_lane'])
+            lane4_result_rank = int(data_row['lane4_result_rank'])
 
-        if np.isnan(data_row['result_rank5_lane']):
+        if np.isnan(data_row['lane5_result_rank']):
             lane5_result_rank = None
         else:
-            lane5_result_rank = int(data_row['result_rank5_lane'])
+            lane5_result_rank = int(data_row['lane5_result_rank'])
 
-        if np.isnan(data_row['result_rank6_lane']):
+        if np.isnan(data_row['lane6_result_rank']):
             lane6_result_rank = None
         else:
-            lane6_result_rank = int(data_row['result_rank6_lane'])
+            lane6_result_rank = int(data_row['lane6_result_rank'])
 
         race_result_grouped_list.append(RaceResultGrouped(
             year=year, race_num=race_num, lane1_racer_id=lane1_racer_id,
